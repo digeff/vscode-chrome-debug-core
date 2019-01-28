5968d89c2ffa02f42d21103257fdb49584a032ab
D <digeff@microsoft.com>
2019-01-27 16:39:05 -0800

    Create app to test html with scripts and expanded script model

---
+++ test/scriptsModelTestApp/composites/out/cshtml-ts-second-script.js
@@ -0,0 +1,42 @@
+"use strict";
+exports.__esModule = true;
+var SharedLibTS = /** @class */ (function () {
+    function SharedLibTS() {
+        this.fileTitle = 'cshtml-ts-second-script';
+        this.currentScriptURL = this.getCurrentScriptURL();
+        this.title = this.fileTitle + " at " + this.currentScriptURL;
+        this.buttonsArea = document.getElementById('buttons');
+    }
+    SharedLibTS.prototype.getCurrentScript = function () {
+        var allScripts = document.getElementsByTagName('script');
+        return allScripts[allScripts.length - 1];
+    };
+    SharedLibTS.prototype.getCurrentScriptURL = function () {
+        return this.getCurrentScript().src;
+    };
+    SharedLibTS.prototype.addButton = function () {
+        var _this = this;
+        var newButton = document.createElement('input');
+        newButton.type = 'button';
+        newButton.value = this.title;
+        newButton.onclick = function () { return _this.onButtonClicked(); };
+        this.buttonsArea.appendChild(newButton);
+        this.buttonsArea.appendChild(document.createElement('br'));
+    };
+    SharedLibTS.prototype.onButtonClicked = function () {
+        console.log("Button from " + this.title + " was clicked message 0");
+        console.log("Button from " + this.title + " was clicked message 1");
+        console.log("Button from " + this.title + " was clicked message 2");
+        console.log("Button from " + this.title + " was clicked message 3");
+        console.log("Button from " + this.title + " was clicked message 4");
+        console.log("Button from " + this.title + " was clicked message 5");
+        console.log("Button from " + this.title + " was clicked message 6");
+        console.log("Button from " + this.title + " was clicked message 7");
+        console.log("Button from " + this.title + " was clicked message 8");
+        console.log("Button from " + this.title + " was clicked message 9");
+    };
+    return SharedLibTS;
+}());
+exports.SharedLibTS = SharedLibTS;
+new SharedLibTS().addButton();
+//# sourceMappingURL=cshtml-ts-second-script.js.map
---
+++ test/scriptsModelTestApp/composites/src/cshtml-ts-first-script.ts
@@ -0,0 +1,56 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+import { Script } from 'vm';
+
+export class SharedLibTS {
+    private readonly fileTitle = 'cshtml-ts-first-script';
+    private readonly currentScriptURL = this.getCurrentScriptURL();
+    private readonly title = `${this.fileTitle} at ${this.currentScriptURL}`;
+    private readonly buttonsArea = document.getElementById('buttons');
+
+    private getCurrentScript(): HTMLScriptElement {
+        const allScripts = document.getElementsByTagName('script');
+        return allScripts[allScripts.length - 1];
+    }
+
+    private getCurrentScriptURL(): string {
+        return this.getCurrentScript().src;
+    }
+
+    public addButton(): void {
+        const newButton = document.createElement('input');
+        newButton.type = 'button';
+        newButton.value = this.title;
+        newButton.onclick = () => this.onButtonClicked();
+
+        this.buttonsArea.appendChild(newButton);
+        this.buttonsArea.appendChild(document.createElement('br'));
+    }
+
+    private onButtonClicked(): void {
+        console.log(`Button from ${this.title} was clicked message 0`);
+        console.log(`Button from ${this.title} was clicked message 1`);
+        console.log(`Button from ${this.title} was clicked message 2`);
+        console.log(`Button from ${this.title} was clicked message 3`);
+        console.log(`Button from ${this.title} was clicked message 4`);
+        console.log(`Button from ${this.title} was clicked message 5`);
+        console.log(`Button from ${this.title} was clicked message 6`);
+        console.log(`Button from ${this.title} was clicked message 7`);
+        console.log(`Button from ${this.title} was clicked message 8`);
+        console.log(`Button from ${this.title} was clicked message 9`);
+    }
+}
+
+new SharedLibTS().addButton();    
